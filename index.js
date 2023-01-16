0440cbd8067725b5dce35f08cc0468eb3e16cc3c
Risad212 <hafez.risad@gmail.com>
2023-01-16 18:50:35 +0600

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 const bodyParser = require("body-parser");
 const AdmZip = require("adm-zip");
 
